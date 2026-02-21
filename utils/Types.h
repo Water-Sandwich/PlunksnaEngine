3cae1248ef433ff84aa1b5b686e1fa41388f1cc9
Water-Sandwich <deivoro23@gmail.com>
2026-02-21 19:00:48 +0000

    before types refactor

---
+++ utils/Types.h
@@ -0,0 +1,30 @@
+//
+// Created by d on 2/21/26.
+//
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <glm/fwd.hpp>
+
+namespace Plunksna {
+
+using glm::i8;
+using glm::i16;
+using glm::i32;
+using glm::i64;
+
+using glm::u8;
+using glm::u16;
+using glm::u32;
+using glm::u64;
+
+//TODO: eventually add f16
+//using glm::f16;
+using glm::f32;
+using glm::f64;
+
+};
+
+
+#endif //TYPES_H
